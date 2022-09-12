f96bd5ec4d178abe6a667f99ec7577d852302d33
Sina-Abf <abf.sina@gmail.com>
2022-09-12 14:27:09 +0430

    add more stylings and images & moving code to seprate component

--- components/sections/HeroSection.jsx
+++ components/sections/HeroSection.jsx
@@ -3,40 +3,13 @@ import Image from 'next/image';
 import Navbar from '../Navbar';
 import Button from '../UI/Button';
 import HeroImage from '../../public/illustrations/globe.png';
+import HeroSectionMain from './HeroSectionMain';
 
 const HeroSection = () => {
   return (
     <section className="min-h-screen min-w-fit bg-primary-custom">
       <Navbar />
-      <div className="grid grid-cols-1 md:grid-cols-2">
-        <div className="px-24 flex flex-col">
-          <div className="mt-16 text-center md:text-start mb-6">
-            <h3 className="font-semibold text-secondary mb-2 md:text-2xl">
-              SIGN UP TODAY
-            </h3>
-            <div className="text-4xl md:text-6xl font-bold">
-              <h1>The World's</h1>
-              <h1 className="text-blue-gradient py-2">Fastest Growing</h1>
-              <h1>Crypto Web App</h1>
-            </div>
-          </div>
-          <p className="max-w-xl mb-8 hidden md:block text-gray">
-            Buy and sell 200+ cryptocurrencies with 20+ flat currencies using
-            bank transfers or your credit/debit card.
-          </p>
-          <div className="flex flex-col md:flex-row gap-y-4 md:gap-x-8 max-w-sm mx-auto md:m-0">
-            <Button className="bg-secondary py-6 text-white min-w-fit hover:bg-transparent hover:text-secondary">
-              Get Started
-            </Button>
-            <Button className="py-6 text-secondary hover:bg-secondary hover:text-white">
-              Download App
-            </Button>
-          </div>
-        </div>
-        <div className="hidden md:block px-8">
-          <Image src={HeroImage} alt="globe" />
-        </div>
-      </div>
+      <HeroSectionMain />
     </section>
   );
 };
---
+++ components/sections/HeroSectionMain.jsx
@@ -0,0 +1,64 @@
+import Button from '../UI/Button';
+import HeroImage from '../../public/illustrations/globe.png';
+import OrangeCircle from '../../public/particles/circle3.svg';
+import PurpleCirlce from '../../public/particles/circle2.svg';
+import BlueCircle from '../../public/particles/circle1.svg';
+import Star from '../../public/particles/star1.svg';
+import Image from 'next/future/image';
+
+const HeroSectionMain = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2">
+      <div className="px-24 flex flex-col">
+        <div className="mt-16 text-center md:text-start mb-6">
+          <h3 className="font-semibold text-secondary mb-2 md:text-2xl">
+            SIGN UP TODAY
+          </h3>
+          <div className="text-4xl md:text-6xl font-bold relative">
+            <h1>The World&apos;s</h1>
+            <h1 className="text-blue-gradient py-2">Fastest Growing</h1>
+            <h1>Crypto Web App</h1>
+            <Image
+              className="absolute top-0 right-[10%] hidden md:block"
+              src={Star}
+              alt="star"
+            />
+          </div>
+        </div>
+        <p className="max-w-xl mb-8 hidden md:block text-gray">
+          Buy and sell 200+ cryptocurrencies with 20+ flat currencies using bank
+          transfers or your credit/debit card.
+        </p>
+        <div className="flex flex-col md:flex-row gap-y-4 md:gap-x-8 max-w-sm mx-auto md:m-0 relative">
+          <Button className="bg-secondary py-6 text-white min-w-fit hover:bg-transparent hover:text-secondary">
+            Get Started
+          </Button>
+          <Button className="py-6 text-secondary hover:bg-secondary hover:text-white">
+            Download App
+          </Button>
+        </div>
+        <div className="hidden md:block p-12 relative">
+          <Image
+            className="absolute bottom-0 left-0"
+            src={PurpleCirlce}
+            alt="purple-circle"
+          />
+        </div>
+      </div>
+      <div className="hidden md:block px-8 relative">
+        <Image src={HeroImage} alt="globe" />
+        <Image
+          className="absolute right-[15%] bottom-[35%]"
+          src={OrangeCircle}
+          alt="orange-circle"
+        />
+        <Image
+          className="absolute top-[5%]"
+          src={BlueCircle}
+          alt="purple-circle"
+        />
+      </div>
+    </div>
+  );
+};
+export default HeroSectionMain;
